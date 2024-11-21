e3ac6eac2e71d1e4e5162d7e10bcdce10953babf
Tomasz Komański <tom.komanski@gmail.com>
2024-11-21 11:54:24 +0100

    To do mark

--- src/frame_parsers/long_frame_wmbus_format_b.rs
+++ src/frame_parsers/long_frame_wmbus_format_b.rs
@@ -4,5 +4,6 @@ use crate::matrices::errors::*;
 pub fn parse(data: &Vec<u8>, key: &Vec<u8>) -> Result<Datagram, ParserError> {
     let _data: &Vec<u8> = data;
     let _key: &Vec<u8> = key;
+    // To do!
     return Err(ParserError::LongFrameWmbusBError);
 }
